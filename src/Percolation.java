a3151f8c376119a16218c9e35f2dde7b78507251
Samet Atdag <samet2@gmail.com>
2013-02-18 15:41:12 +0200

    Fixed points in overview of Coursera.

--- src/Percolation.java
+++ src/Percolation.java
@@ -12,202 +12,195 @@
  *----------------------------------------------------------------*/
 
 public class Percolation {
-	
-	// WeightedQuickUnionUF data structure.
-	private WeightedQuickUnionUF weightedQuickUnionUF;
-	
-	// A boolean array to mark if element is open or not. 
-	private boolean[] isOpenList;
-	
-	// This value will be assigned to N in Percolation constructor.
-	private int givenN;
-	
-	// Index of virtualBottom element in weightedQuickUnionFind data structure.
-	private int virtualBottomIndex = 0;
-	
-	/**
-	 *   Percolation data structure object constructor.
-	 * 
-	 * 	 @param int N for NxN grid.
-	 */
-	public Percolation(int N) {
 
-		givenN = N;
-		
-		/* We want N rows, N columns. 
-		   Plus, 1 for virtual-top, 1 for virtual-bottom.*/
-		int numberOfArrayElementsInQuickUnionFind = N * N + 2;
-		
-		isOpenList = new boolean[numberOfArrayElementsInQuickUnionFind];
-	
-		virtualBottomIndex = numberOfArrayElementsInQuickUnionFind - 1;
-		
-		isOpenList[0] = true;
-		isOpenList[virtualBottomIndex] = true;
-		
-		// Initialize weightedQuickUnionUF object.
-		weightedQuickUnionUF = 
-				new WeightedQuickUnionUF(numberOfArrayElementsInQuickUnionFind);
-	}
-	
-	/**
-	 *   Opens the site on i-th row and j-th column.
-	 *
-	 *   @param int i: 1-based row index.
-	 *   @param int j: 1-based column index.
-	 *
-	 *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
-	 *   greater than grid size N.
-	 */
-	public void open(int i, int j) {
-		checkBoundaries(i, j);
-		int index = xyToIndex(i, j);
-		isOpenList[index] = true;
-		connectElementToOpenNeighbors(i, j);
-	}
-	
-	/**
-	 *   Returns if the site on i-th row and j-th column is open.
-	 *
-	 *   @param int i: 1-based row index.
-	 *   @param int j: 1-based column index.
-	 *
-	 *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
-	 *   greater than grid size N.
-	 */
-	public boolean isOpen(int i, int j) {
-		checkBoundaries(i,j);
-		return isOpenList[xyToIndex(i, j)];
-	}
-	
-	/**
-	 *   Returns if the site on i-th row and j-th column is connected to 
-	 *   virtualTop element. 
-	 *
-	 *   @param int i: 1-based row index.
-	 *   @param int j: 1-based column index.
-	 *
-	 *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
-	 *   greater than grid size N.
-	 */
-	public boolean isFull(int i, int j) {
-		checkBoundaries(i,j);
-		return weightedQuickUnionUF.connected(0, xyToIndex(i, j));
-			
-	}
-	
-	/**
-	 *   Returns if Percolation object percolates by testing if virtualTop
-	 *   and virtualBottom is connected. 
-	 *
-	 */
-	public boolean percolates() {
-		return weightedQuickUnionUF.connected(0, virtualBottomIndex);
-	}
-	
-	/**
-	 *   Test client for testing Percolation object.
-	 *
-	 */
-	public static void main(String[] args) {
-		Percolation p = new Percolation(10);
-		StdOut.println("Done.");
+    // WeightedQuickUnionUF data structure.
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+    
+    // A boolean array to mark if element is open or not. 
+    private boolean[] isOpenList;
+    
+    // This value will be assigned to N in Percolation constructor.
+    private int givenN;
+    
+    // Index of virtualBottom element in weightedQuickUnionFind data structure.
+    private int virtualBottomIndex = 0;
+    
+    /**
+     *   Percolation data structure object constructor.
+     * 
+     *      @param int N for NxN grid.
+     */
+    public Percolation(int N) {
 
-	}
-	
-	/**
-	 *   Checks the row and column index by boundary conditions.  
-	 *
-	 *   @param int i: 1-based row index.
-	 *   @param int j: 1-based column index.
-	 *
-	 *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
-	 *   greater than grid size N.
-	 */
-	private void checkBoundaries(int row, int column) {
-		if (row <= 0 || row > givenN) 
-			throw new IndexOutOfBoundsException("row index i out of bounds");
-		if (column <= 0 || column > givenN) 
-			throw new IndexOutOfBoundsException("column index j out of bounds");
-	}
-	
-	/**
-	 *   Convert 2-D array to 1-D array representation..  
-	 *
-	 *   @param int i: 1-based row index.
-	 *   @param int j: 1-based column index.
-	 *
-	 */
-	private int xyToIndex(int row, int column) {
-		return ((row-1) * givenN) + column;
-	}
-	
-	/**
-	 *   Connect siteIndex site to [neighborRow,neighborColumn] site.  
-	 *
-	 *   @param int siteIndex: Index of site in 1-D array.
-	 *   @param int neighborRow: 1-based row index.
-	 *   @param int neighborColumn: 1-based column index.
-	 *
-	 *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
-	 *   greater than grid size N.
-	 */
-	private void connectSiteToNeighbor(int siteIndex, int neighborRow, 
-			int neighborColumn) {
-		int neighborIndex = xyToIndex(neighborRow, neighborColumn);
-		weightedQuickUnionUF.union(siteIndex, neighborIndex);
-	}
-	
-	/**
-	 *   Connect site on [row,column] to all open neighbors.  
-	 *
-	 *   @param int i: 1-based row index.
-	 *   @param int j: 1-based column index.
-	 *
-	 *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
-	 *   greater than grid size N.
-	 */
-	private void connectElementToOpenNeighbors(int row, int column) {
-		int siteIndex = xyToIndex(row, column);
-		
-		if (row == 1) {
-			weightedQuickUnionUF.union(0, siteIndex);
-		}
-		
-		if (row == givenN) {
-			weightedQuickUnionUF.union(virtualBottomIndex, siteIndex);
-		}
-		
-		
-		/* Let's find neighbors of given element. 
-		 Check border conditions, if neighbor is open also, connect them.*/
-		if (row != 1) { // Above neighbor
-			if (isOpen(row-1, column)) {
-				connectSiteToNeighbor(siteIndex, row-1, column);
-			}
-		} 
-		if (row != givenN) { // Below neighbor 
-			if (isOpen(row+1, column)) {
-				connectSiteToNeighbor(siteIndex, row+1, column);
-			}
-		} else {
-			
-		}
-		
-		if (column != 1) { // Left neighbor
-			if (isOpen(row, column-1)) {
-				connectSiteToNeighbor(siteIndex, row, column-1);
-			}
-		} 
-		if (column != givenN) { // Right neighbor
-			if (isOpen(row, column+1)) {
-				connectSiteToNeighbor(siteIndex, row, column+1);
-			}
-		} else {
-			
-		}
-	}
-	
+        givenN = N;
+        
+        /* We want N rows, N columns. 
+           Plus, 1 for virtual-top, 1 for virtual-bottom.*/
+        int numberOfArrayElements = N * N + 2;
+        
+        isOpenList = new boolean[numberOfArrayElements];
+    
+        virtualBottomIndex = numberOfArrayElements - 1;
+        
+        isOpenList[0] = true;
+        isOpenList[virtualBottomIndex] = true;
+        
+        // Initialize weightedQuickUnionUF object.
+        weightedQuickUnionUF = 
+                new WeightedQuickUnionUF(numberOfArrayElements);
+    }
+    
+    /**
+     *   Opens the site on i-th row and j-th column.
+     *
+     *   @param int i: 1-based row index.
+     *   @param int j: 1-based column index.
+     *
+     *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
+     *   greater than grid size N.
+     */
+    public void open(int i, int j) {
+        checkBoundaries(i, j);
+        int index = xyToIndex(i, j);
+        isOpenList[index] = true;
+        connectElementToOpenNeighbors(i, j);
+    }
+    
+    /**
+     *   Returns if the site on i-th row and j-th column is open.
+     *
+     *   @param int i: 1-based row index.
+     *   @param int j: 1-based column index.
+     *
+     *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
+     *   greater than grid size N.
+     */
+    public boolean isOpen(int i, int j) {
+        checkBoundaries(i, j);
+        return isOpenList[xyToIndex(i, j)];
+    }
+    
+    /**
+     *   Returns if the site on i-th row and j-th column is connected to 
+     *   virtualTop element. 
+     *
+     *   @param int i: 1-based row index.
+     *   @param int j: 1-based column index.
+     *
+     *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
+     *   greater than grid size N.
+     */
+    public boolean isFull(int i, int j) {
+        checkBoundaries(i, j);
+        return weightedQuickUnionUF.connected(0, xyToIndex(i, j));
+            
+    }
+    
+    /**
+     *   Returns if Percolation object percolates by testing if virtualTop
+     *   and virtualBottom is connected. 
+     *
+     */
+    public boolean percolates() {
+        return weightedQuickUnionUF.connected(0, virtualBottomIndex);
+    }
+    
+    /**
+     *   Test client for testing Percolation object.
+     *
+     */
+    public static void main(String[] args) {
+        Percolation p = new Percolation(10);
+        StdOut.println("Done.");
 
-
+    }
+    
+    /**
+     *   Checks the row and column index by boundary conditions.  
+     *
+     *   @param int i: 1-based row index.
+     *   @param int j: 1-based column index.
+     *
+     *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
+     *   greater than grid size N.
+     */
+    private void checkBoundaries(int row, int column) {
+        if (row <= 0 || row > givenN) 
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+        if (column <= 0 || column > givenN) 
+            throw new IndexOutOfBoundsException("column index j out of bounds");
+    }
+    
+    /**
+     *   Convert 2-D array to 1-D array representation..  
+     *
+     *   @param int i: 1-based row index.
+     *   @param int j: 1-based column index.
+     *
+     */
+    private int xyToIndex(int row, int column) {
+        return ((row-1) * givenN) + column;
+    }
+    
+    /**
+     *   Connect siteIndex site to [neighborRow,neighborColumn] site.  
+     *
+     *   @param int siteIndex: Index of site in 1-D array.
+     *   @param int neighborRow: 1-based row index.
+     *   @param int neighborColumn: 1-based column index.
+     *
+     *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
+     *   greater than grid size N.
+     */
+    private void connectSiteToNeighbor(int siteIndex, int neighborRow, 
+            int neighborColumn) {
+        int neighborIndex = xyToIndex(neighborRow, neighborColumn);
+        weightedQuickUnionUF.union(siteIndex, neighborIndex);
+    }
+    
+    /**
+     *   Connect site on [row,column] to all open neighbors.  
+     *
+     *   @param int i: 1-based row index.
+     *   @param int j: 1-based column index.
+     *
+     *   Throws a IndexOutOfBoundsException if i or j is smaller than 0 or 
+     *   greater than grid size N.
+     */
+    private void connectElementToOpenNeighbors(int row, int column) {
+        int siteIndex = xyToIndex(row, column);
+        
+        if (row == 1) {
+            weightedQuickUnionUF.union(0, siteIndex);
+        }
+        
+        if (row == givenN) {
+            weightedQuickUnionUF.union(virtualBottomIndex, siteIndex);
+        }
+        
+        
+        /* Let's find neighbors of given element. 
+         Check border conditions, if neighbor is open also, connect them.*/
+        if (row != 1) { // Above neighbor
+            if (isOpen(row-1, column)) {
+                connectSiteToNeighbor(siteIndex, row-1, column);
+            }
+        } 
+        if (row != givenN) { // Below neighbor 
+            if (isOpen(row+1, column)) {
+                connectSiteToNeighbor(siteIndex, row+1, column);
+            }
+        }
+        
+        if (column != 1) { // Left neighbor
+            if (isOpen(row, column-1)) {
+                connectSiteToNeighbor(siteIndex, row, column-1);
+            }
+        } 
+        if (column != givenN) { // Right neighbor
+            if (isOpen(row, column+1)) {
+                connectSiteToNeighbor(siteIndex, row, column+1);
+            }
+        }
+    }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -15,120 +15,121 @@
  *
  *----------------------------------------------------------------*/
 public class PercolationStats {
-	
-	// Percolation object to get statistics.
-	private Percolation p;
-	
-	// To avoid same row,column pairs, we hold the already tried pairs in this
-	// array.
-	private int[][] triedRowColumnPairs;
-	
-	// Number of open() calls. (without retries)
-	private double numberOfTries = 0;
-	
-	// Result of each experiment is hold in this array.
-	private double[] monteCarloResults;
-	
-	// Parameter T which is given at creation.
-	private int mT;
-	
-	/**
-	 *   Initializes PercolationStats, with NxN size Percolation Object and 
-	 *   T-times trying Monte Carlo simulation.
-	 *
-	 *   Throws a IllegalArgumentException if N or T is smaller then 1.
-	 */
-	public PercolationStats(int N, int T) {
-		if (N<=0 || T<=0) {
-			throw 
-				new IllegalArgumentException("N and T must be bigger than 0.");
-		}
-		
-		mT = T;
-		monteCarloResults = new double[T];
-		
-		
-		/* StdRandom.uniform calls can produce same row:column pairs. 
-		   Let's try again if row:column pair is tried before. 
-		   I assume at N*N*2 calls are enough for percolation. */
-		for (int i=0; i<T; i++) {
-			
-			// Initialize Monte Carlo variables for experiment:
-			p = new Percolation(N);
-			numberOfTries = 0.0;
-			triedRowColumnPairs = new int[N][N];
-			
-			// Let's begin the experiment.
-			for (int j=0; j<N*N*2; j++) {
-				int row = StdRandom.uniform(N) + 1;
-				int column = StdRandom.uniform(N) + 1;
-				if (triedRowColumnPairs[row-1][column-1] == 1) {
-					continue;
-				} else {
-					triedRowColumnPairs[row-1][column-1] = 1;
-					numberOfTries++;
-					p.open(row, column);
-					if (p.percolates()) {
-						monteCarloResults[i] = numberOfTries/(N*N);
-						
-						/* If it percolates, stop this experiment and continue
-						   to next experiment.*/
-						break;
-					} 
-				}
-			}
-		}
-	}
-	
-	/**
-	 *   Calculates and returns arithmetic average of Monte Carlo simulation
-	 *   results.
-	 *
-	 */
-	public double mean() {
-		return StdStats.mean(monteCarloResults);
-	}
-	
-	/**
-	 *   Calculates and returns standard deviation of Monte Carlo simulation
-	 *   results. 
-	 *
-	 */
-	public double stddev() {
-		return StdStats.stddev(monteCarloResults);
-	}
-	
-	/**
-	 *   Calculates and returns lower bound of the 95% confidence interval 
-	 *   using the formula given in assignment definition
-	 *
-	 */
-	public double confidenceLo() {
-		return mean() - ((1.96 * stddev()) / Math.sqrt(mT));
-	}
-	
-	/**
-	 *   Calculates and returns upper bound of the 95% confidence interval 
-	 *   using the formula given in assignment definition
-	 *
-	 */
-	public double confidenceHi() {
-		return mean() + ((1.96 * stddev()) / Math.sqrt(mT));
-	}
-	
-	/**
-	 *   Test client for testing PercolationStats object.
-	 *
-	 *   Throws a IllegalArgumentException if first or second argument is 
-	 *   smaller then 1.
-	 */
-	public static void main(String[] args) {
-		int N = Integer.parseInt(args[0]);
-		int T = Integer.parseInt(args[1]);
-		PercolationStats pstats = new PercolationStats(N, T);
-		StdOut.println("mean\t\t\t = " + pstats.mean());
-		StdOut.println("stddev\t\t\t = " + pstats.stddev());
-		StdOut.println("95% confidence interval\t = " + pstats.confidenceLo()
-				+ ", " + pstats.confidenceHi());
-	}
+    
+    // Percolation object to get statistics.
+    private Percolation p;
+    
+    // To avoid same row,column pairs, we hold the already tried pairs in this
+    // array.
+    private int[][] triedRowColumnPairs;
+    
+    // Number of open() calls. (without retries)
+    private double numberOfTries = 0;
+    
+    // Result of each experiment is hold in this array.
+    private double[] monteCarloResults;
+    
+    // Parameter T which is given at creation.
+    private int mT;
+    
+    /**
+     *   Initializes PercolationStats, with NxN size Percolation Object and 
+     *   T-times trying Monte Carlo simulation.
+     *
+     *   Throws a IllegalArgumentException if N or T is smaller then 1.
+     */
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0) {
+            throw 
+                new IllegalArgumentException("N, T must be bigger than 0.");
+        }
+        
+        mT = T;
+        monteCarloResults = new double[T];
+        
+        
+        /* StdRandom.uniform calls can produce same row:column pairs. 
+           Let's try again if row:column pair is tried before. 
+           I assume at N*N*2 calls are enough for percolation. */
+        for (int i = 0; i < T; i++) {
+            
+            // Initialize Monte Carlo variables for experiment:
+            p = new Percolation(N);
+            numberOfTries = 0.0;
+            triedRowColumnPairs = new int[N][N];
+            
+            // Let's begin the experiment.
+            for (int j = 0; j < N * N * 2; j++) {
+                int row = StdRandom.uniform(N) + 1;
+                int column = StdRandom.uniform(N) + 1;
+                if (triedRowColumnPairs[row-1][column-1] == 1) {
+                    continue;
+                } else {
+                    triedRowColumnPairs[row-1][column-1] = 1;
+                    numberOfTries++;
+                    p.open(row, column);
+                    if (p.percolates()) {
+                        monteCarloResults[i] = numberOfTries/(N*N);
+                        
+                        /* If it percolates, stop this 
+                           experiment and continue to next 
+                           experiment.*/
+                        break;
+                    } 
+                }
+            }
+        }
+    }
+    
+    /**
+     *   Calculates and returns arithmetic average of Monte Carlo simulation
+     *   results.
+     *
+     */
+    public double mean() {
+        return StdStats.mean(monteCarloResults);
+    }
+    
+    /**
+     *   Calculates and returns standard deviation of Monte Carlo simulation
+     *   results. 
+     *
+     */
+    public double stddev() {
+        return StdStats.stddev(monteCarloResults);
+    }
+    
+    /**
+     *   Calculates and returns lower bound of the 95% confidence interval 
+     *   using the formula given in assignment definition
+     *
+     */
+    public double confidenceLo() {
+        return mean() - ((1.96 * stddev()) / Math.sqrt(mT));
+    }
+    
+    /**
+     *   Calculates and returns upper bound of the 95% confidence interval 
+     *   using the formula given in assignment definition
+     *
+     */
+    public double confidenceHi() {
+        return mean() + ((1.96 * stddev()) / Math.sqrt(mT));
+    }
+    
+    /**
+     *   Test client for testing PercolationStats object.
+     *
+     *   Throws a IllegalArgumentException if first or second argument is 
+     *   smaller then 1.
+     */
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        PercolationStats pstats = new PercolationStats(N, T);
+        StdOut.println("mean\t\t\t = " + pstats.mean());
+        StdOut.println("stddev\t\t\t = " + pstats.stddev());
+        StdOut.println("95% confidence interval\t = " + pstats.confidenceLo()
+                + ", " + pstats.confidenceHi());
+    }
 }
