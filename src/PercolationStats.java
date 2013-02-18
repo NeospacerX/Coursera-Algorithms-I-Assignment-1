1c410c50fa4c13aeeb469d1c3aefef0935091c75
Samet <developer@ayorhan.local>
2013-02-18 12:05:28 +0200

    Added comments, class and function documentation

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,23 +1,57 @@
+/*----------------------------------------------------------------
+ *  Author:        Samet Atdag
+ *  Written:       15/2/2013
+ *  Last updated:  18/2/2013
+ *
+ *  Compilation:   javac PercolationStats.java
+ *  Execution:     java PercolationStats
+ *  
+ *  Statistics tools for Percolation data structure.  
+ *
+ * % java PercolationStats 2 10000
+ * mean                    = 0.666925
+ * stddev                  = 0.11776536521033558
+ * 95% confidence interval = 0.6646167988418774, 0.6692332011581226
+ *
+ *----------------------------------------------------------------*/
 public class PercolationStats {
+	
+	// Percolation object to get statistics.
 	Percolation p;
+	
+	// To avoid same row,column pairs, we hold the already tried pairs in this
+	// array.
 	int[][] triedRowColumnPairs;
+	
+	// Number of open() calls. (without retries)
 	double numberOfTries = 0;
+	
+	// Result of each experiment is hold in this array.
 	double[] monteCarloResults;
+	
+	// Parameter T which is given at creation.
 	int mT;
 	
+	/**
+	 *   Initializes PercolationStats, with NxN size Percolation Object and 
+	 *   T-times trying Monte Carlo simulation.
+	 *
+	 *   Throws a IllegalArgumentException if N or T is smaller then 1.
+	 */
 	public PercolationStats(int N, int T) {
 		if (N<=0 || T<=0) {
-			throw new IllegalArgumentException("N and T must be bigger than 0.");
+			throw 
+				new IllegalArgumentException("N and T must be bigger than 0.");
 		}
+		
 		mT = T;
 		monteCarloResults = new double[T];
 		
 		
-		// StdRandom.uniform calls can produce same row:column pairs. 
-		// Let's try again if row:column pair is tried before. 
-		// I assume at N*N*2 calls are enough for percolation.
+		/* StdRandom.uniform calls can produce same row:column pairs. 
+		   Let's try again if row:column pair is tried before. 
+		   I assume at N*N*2 calls are enough for percolation. */
 		for (int i=0; i<T; i++) {
-			
 			
 			// Initialize Monte Carlo variables for experiment:
 			p = new Percolation(N);
@@ -35,37 +69,66 @@ public class PercolationStats {
 					numberOfTries++;
 					p.open(row, column);
 					if (p.percolates()) {
-						StdOut.println("Percolates at: " + numberOfTries);
 						monteCarloResults[i] = numberOfTries/(N*N);
 						
-						// If it percolates, stop this experiment and continue
-						// to next experiment.
+						/* If it percolates, stop this experiment and continue
+						   to next experiment.*/
 						break;
 					} 
 				}
 			}
 		}
 	}
+	
+	/**
+	 *   Calculates and returns arithmetic average of Monte Carlo simulation
+	 *   results.
+	 *
+	 */
 	public double mean() {
 		return StdStats.mean(monteCarloResults);
 	}
+	
+	/**
+	 *   Calculates and returns standard deviation of Monte Carlo simulation
+	 *   results. 
+	 *
+	 */
 	public double stddev() {
 		return StdStats.stddev(monteCarloResults);
 	}
 	
-	// Used the formula given in assignment definition.
+	/**
+	 *   Calculates and returns lower bound of the 95% confidence interval 
+	 *   using the formula given in assignment definition
+	 *
+	 */
 	public double confidenceLo() {
 		return mean() - ((1.96 * stddev()) / Math.sqrt(mT));
 	}
 	
-	// Used the formula given in assignment definition.
+	/**
+	 *   Calculates and returns upper bound of the 95% confidence interval 
+	 *   using the formula given in assignment definition
+	 *
+	 */
 	public double confidenceHi() {
 		return mean() + ((1.96 * stddev()) / Math.sqrt(mT));
 	}
+	
+	/**
+	 *   Test client for testing PercolationStats object.
+	 *
+	 *   Throws a IllegalArgumentException if first or second argument is 
+	 *   smaller then 1.
+	 */
 	public static void main(String[] args) {
-		PercolationStats pstats = new PercolationStats(20, 11);
+		int N = Integer.parseInt(args[0]);
+		int T = Integer.parseInt(args[1]);
+		PercolationStats pstats = new PercolationStats(N, T);
 		StdOut.println("mean\t\t\t = " + pstats.mean());
 		StdOut.println("stddev\t\t\t = " + pstats.stddev());
-		StdOut.println("95% confidence interval\t = " + pstats.confidenceLo() + ", " + pstats.confidenceHi());
+		StdOut.println("95% confidence interval\t = " + pstats.confidenceLo()
+				+ ", " + pstats.confidenceHi());
 	}
 }
